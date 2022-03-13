64aaf84d7f2bd73dd309f7400ed783c66d63b8f7
Max <academy@neotech.srl>
2022-03-13 18:53:00 +0100

    Formine

--- ClassModel_Formine/src/srl/neotech/academy/formine/StartApplication.java
+++ ClassModel_Formine/src/srl/neotech/academy/formine/StartApplication.java
@@ -1,5 +1,13 @@
 package srl.neotech.academy.formine;
 
+
+/*Esempio di Ereditarietà e Poliformismo
+ * 
+ * gli oggetti nemo e castello eseguono metodi specializzati: aggiungicoda() e aggiungifinestra()
+ * inoltre eseguono tutti e due il metodo creaForma() dell'interfaccia comune che viene specializzata
+ * nell'implementazione
+ */
+
 public class StartApplication {
 
 	public static void main(String[] args) {
