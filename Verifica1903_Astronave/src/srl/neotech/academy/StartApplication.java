6e5506912c512f5a6b159c8af4949514b7a3a8f8
Max <academy@neotech.srl>
2022-03-25 19:15:55 +0100

    prova conflitto2

--- Verifica1903_Astronave/src/srl/neotech/academy/StartApplication.java
+++ Verifica1903_Astronave/src/srl/neotech/academy/StartApplication.java
@@ -6,7 +6,9 @@ public class StartApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("sto sul brach1!!!!");
+		System.out.println("sto sul brach1!!!!");
+		System.out.println("sto sul brach1!!!!");
 		System.out.println("sto sul brach1!!!!");
 		Astronave xwing=new Astronave();
 		Scanner scan=new Scanner(System.in);
