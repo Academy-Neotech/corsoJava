1e6f89238a582a74ab8b1e664e8486c61a8eb163
Max <academy@neotech.srl>
2022-03-26 09:59:10 +0100

    test

---
+++ TestMaven/src/main/java/srl/neotech/academy/StartApplication.java
@@ -0,0 +1,16 @@
+package srl.neotech.academy;
+
+import org.kohsuke.randname.RandomNameGenerator;
+
+public class StartApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		RandomNameGenerator rnd = new RandomNameGenerator(0);
+		
+		
+		System.out.println(rnd.next());
+	}
+
+}
