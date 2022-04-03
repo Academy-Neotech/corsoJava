51c6ce68e635c927d3c7140472e22dea056c6703
Max <academy@neotech.srl>
2022-04-03 10:08:11 +0200

    aggiunto template webApp

--- TestMaven/src/main/java/srl/neotech/academy/StartApplication.java
+++ TestMaven/src/main/java/srl/neotech/academy/StartApplication.java
@@ -1,16 +1,41 @@
 package srl.neotech.academy;
+import static com.diogonunes.jcolor.Ansi.colorize;
+import static com.diogonunes.jcolor.Attribute.MAGENTA_BACK;
+import static com.diogonunes.jcolor.Attribute.YELLOW_TEXT;
 
-import org.kohsuke.randname.RandomNameGenerator;
-
+import java.util.ArrayList;
 public class StartApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	
+		
+		Motocicletta ducatiMonster=new Motocicletta();
+		ducatiMonster.setMarca("Ducati");
+		ducatiMonster.setModello("Monster");
+		
+		ArrayList<Motocicletta> motociclette=new ArrayList<Motocicletta>();
+		
+		PompaDiBenzina benzinaioCentocelle=new PompaDiBenzina();
+		benzinaioCentocelle.faiIlPieno(ducatiMonster);
+		
+		//System.out.println(ducatiMonster.getMarca());
+		//System.out.println(ducatiMonster.getLivelloBenzina());
+		
+		DepositoGiudiziario deposito=new DepositoGiudiziario();
+		
+		motociclette.add(ducatiMonster);
 
-		RandomNameGenerator rnd = new RandomNameGenerator(0);
+		
+		for(String pippo : deposito.getTarghe()) {
+			System.out.println(pippo);
+		}
+		
+		for(Motocicletta pippo: motociclette) {
+			System.out.println(pippo.getMarca());
+		}
 		
 		
-		System.out.println(rnd.next());
 	}
 
 }
